3475fc87dd4b500a1bd55ef9ea26d310851abdea
HugoDrmn <hugo.darmon91@gmail.com>
2019-09-05 10:20:26 +0200

    1er Commit

--- SortieStandard/main.cpp
+++ SortieStandard/main.cpp
@@ -8,10 +8,14 @@ using namespace std;
 int main()
 {
    string nomDuFichier;
+   string nomDuFichier2;
    cout<<"Entrer le nom du fichier à lire:";
    cin>>nomDuFichier;
+   cout<<"Entrer le nom du fichier à écrire:";
+   cin>>nomDuFichier2;
    //Créationdufluxenlecturesurlefichier
    ifstream leFichier(nomDuFichier.c_str());// c_str() transforme string en char*
+   ofstream fichier(nomDuFichier2.c_str());
    if(!leFichier.is_open())
        cerr<<"Erreur lors de l'ouverture du fichier"<<endl;
    else
@@ -20,17 +24,19 @@ int main()
        int nbOr;
        int nbArgent;
        int nbBronze;
-       cout<<"+"<<setfill('-')<<setw(21)<<"+"<<setfill('-')<<setw(9)<<"+"<<setfill('-')<<setw(9)<<"+"<<setfill('-')<<setw(9)<<"+"<<endl;
+       fichier<<"+"<<setfill('-')<<setw(17)<<"+"<<setfill('-')<<setw(9)<<"+"<<setfill('-')<<setw(9)<<"+"<<setfill('-')<<setw(9)<<"+"<<endl;
        do
        {
            //récupération des valeurs
            leFichier>>pays>>nbOr>>nbArgent>>nbBronze;
            if(leFichier.good())//Si les valeurs ont bien été lues
            {
-               cout<<"|"<<pays<<"|"<<nbOr<<"|"<<nbArgent<<"|"<<nbBronze<<"|"<<endl;
+               fichier << "|"<<setfill(' ') << setw(16) << left << pays << "|" << setw(8) << right << nbOr << "|" << setw(8) << right << nbArgent << "|"  << setw(8) << right << nbBronze << "|" << endl ;
            }
        }while(!leFichier.eof());
-       cout<<"+"<<setfill('-')<<setw(21)<<"+"<<setfill('-')<<setw(9)<<"+"<<setfill('-')<<setw(9)<<"+"<<setfill('-')<<setw(9)<<"+"<<endl;
+       fichier<<"+"<<setfill('-')<<setw(17)<<"+"<<setfill('-')<<setw(9)<<"+"<<setfill('-')<<setw(9)<<"+"<<setfill('-')<<setw(9)<<"+"<<endl;
    }
+   leFichier.close();
+   fichier.close();
    return 0;
 }
